ae44a39ae6174d559fbbc8724fb9dec63d6f1288
Ashwini kumar sinha <31278395+ASHWINISINHA@users.noreply.github.com>
2018-06-15 20:16:30 +0530

    Update 2run.py

--- 2run.py
+++ 2run.py
@@ -4,7 +4,7 @@ from time import sleep
 y=(0.1)
 subprocess.Popen(["python", 'rd1.py'])
 sleep(y)
-subprocess.Popen(["python", 'rs2.py'])
+subprocess.Popen(["python", 'rs1.py'])
 sleep(y)
 subprocess.Popen(["python", 'timeled.py'])
 
